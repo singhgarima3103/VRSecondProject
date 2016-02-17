76575c99690cb5c85fa44c3d5020fcaa314a4534
jtzhang2015 <jtzhang@ufl.edu>
2016-02-16 20:42:02 -0500

    First scene (1.00) and sound package

---
+++ Assets/Scripts/Player/Player/CameraShake.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour {
+
+    public float amplitude = 0.2f;
+    bool isShake = false;
+    Transform headLocation;
+
+    void Start()
+    {
+        headLocation = GetComponent<Transform>();
+    }
+
+    void OnTriggerEnter()
+    {
+        isShake = true;
+    }
+
+    void OnTriggerExit()
+    {
+        isShake = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (isShake)
+        {
+            Vector3 shakeDirection = Random.insideUnitSphere;
+            shakeDirection.y = 0;
+            headLocation.position += shakeDirection * amplitude;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player/Trigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+
+    Ray playerRay;
+    RaycastHit playerRaycastHit;
+    int range = 10;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Interact()
+    {
+        playerRay.origin = GetComponent<Transform>().position;
+        playerRay.direction = GetComponent<Transform>().forward;
+        Physics.Raycast(playerRay, out playerRaycastHit, range);
+        // playerRaycastHit.collider.
+
+    }
+}
