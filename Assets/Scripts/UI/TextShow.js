1b0543177be8ed3839547bfd605d5aaa6b6f2b3e
gargankur2008 <gargankur20011@gmail.com>
2016-02-19 12:35:10 -0500

    Meta File
    Remove

---
+++ Assets/Scripts/UI/TextShow.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var Obj : GameObject;
+
+function Start () {
+
+Obj.SetActive(false);
+
+}
+
+function OnTriggerEnter () { Obj.SetActive (true); }
+
+function OnTriggerExit () { Obj.SetActive(false); }
