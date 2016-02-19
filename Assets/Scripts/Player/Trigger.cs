ee21627b47fe691ca42ad0f4721dc21f2ba5320c
gargankur2008 <gargankur20011@gmail.com>
2016-02-19 15:31:12 -0500

    Work on Scene 1.0 only
    Do not make more scenes. It will get confusing

--- Assets/Scripts/Player/Trigger.cs
+++ Assets/Scripts/Player/Trigger.cs
@@ -1,29 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class Trigger : MonoBehaviour {
 
     Ray playerRay;
     RaycastHit playerRaycastHit;
-    int range = 10;
+    int range = 5;
+    public Text ankylosaurus_info;
+    public Text brontosaurus_info;
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Interact();
+        }
+    }
 
     void Interact()
     {
         playerRay.origin = GetComponent<Transform>().position;
         playerRay.direction = GetComponent<Transform>().forward;
         Physics.Raycast(playerRay, out playerRaycastHit, range);
-        // playerRaycastHit.collider.
+        GameObject aim = playerRaycastHit.collider.gameObject;
+        //ankylosaurus_info.gameObject.SetActive(true);
+        //aim.SetActive(false);
+		if ( aim.tag == "Ankylosaurus")
+        {
+            ankylosaurus_info.gameObject.SetActive(true);
+        }
+		if (aim.CompareTag("Brontosaurus"))
+        {
+            brontosaurus_info.gameObject.SetActive(true);
+        }
+
+        
 
     }
 }
