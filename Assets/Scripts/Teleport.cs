09c9dd7157e4d8483dc029c6b0a8c2061d61f0ef
jtzhang2015 <jtzhang@ufl.edu>
2016-02-20 22:26:22 -0500

    teleport and follow

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+    public Transform target;
+
+
+	
+    void OnTriggerEnter( Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.transform.position = target.transform.position;
+        }
+    }
+}
