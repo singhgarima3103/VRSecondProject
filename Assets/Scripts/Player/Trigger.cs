c1e22290220d996ade14e96d693e9bb291fd097e
gargankur2008 <gargankur20011@gmail.com>
2016-02-22 14:06:17 -0500

    Trigger_Script

--- Assets/Scripts/Player/Trigger.cs
+++ Assets/Scripts/Player/Trigger.cs
@@ -17,6 +17,15 @@ public class Trigger : MonoBehaviour {
 	public Text stegosaurus_info;
 	public Text spinosaurus_info;
 	public Text parasaurolophus_info;
+	public AudioSource ankylosaurus_audio;
+	public AudioSource brontosaurus_audio;
+	public AudioSource carnotaurus_audio;
+	public AudioSource velociraptor_audio;
+	public AudioSource triceratops_audio;
+	public AudioSource tiranosaurus_audio;
+	public AudioSource stegosaurus_audio;
+	public AudioSource spinosaurus_audio;
+	public AudioSource parasaurolophus_audio;
 
 
     public Image background;
@@ -39,46 +48,55 @@ public class Trigger : MonoBehaviour {
         //aim.SetActive(false);
 		if ( aim.tag == "Ankylosaurus")
         {
+			ankylosaurus_audio.gameObject.SetActive (true);
             ankylosaurus_info.gameObject.SetActive(true);
             background.gameObject.SetActive(true);
         }
 		if (aim.CompareTag("Brontosaurus"))
         {
+			brontosaurus_audio.gameObject.SetActive (true);
             brontosaurus_info.gameObject.SetActive(true);
             background.gameObject.SetActive(true);
         }
 		if (aim.CompareTag("Carnotaurus"))
 		{
+			carnotaurus_audio.gameObject.SetActive (true);
 			carnotaurus_info.gameObject.SetActive(true);
 			background.gameObject.SetActive(true);
 		}
 		if (aim.CompareTag("Velociraptor"))
 		{
+			velociraptor_audio.gameObject.SetActive (true);
 			velociraptor_info.gameObject.SetActive(true);
 			background.gameObject.SetActive(true);
 		}
 		if (aim.CompareTag("Triceratops"))
 		{
+			triceratops_audio.gameObject.SetActive (true);
 			triceratops_info.gameObject.SetActive(true);
 			background.gameObject.SetActive(true);
 		}
 		if (aim.CompareTag("Tiranosaurus"))
 		{
+			tiranosaurus_audio.gameObject.SetActive (true);
 			tiranosaurus_info.gameObject.SetActive(true);
 			background.gameObject.SetActive(true);
 		}
 		if (aim.CompareTag("Stegosaurus"))
 		{
+			stegosaurus_audio.gameObject.SetActive (true);
 			stegosaurus_info.gameObject.SetActive(true);
 			background.gameObject.SetActive(true);
 		}
 		if (aim.CompareTag("Spinosaurus"))
 		{
+			spinosaurus_audio.gameObject.SetActive (true);
 			spinosaurus_info.gameObject.SetActive(true);
 			background.gameObject.SetActive(true);
 		}
 		if (aim.CompareTag("Parasaurolophus"))
 		{
+			parasaurolophus_audio.gameObject.SetActive (true);
 			parasaurolophus_info.gameObject.SetActive(true);
 			background.gameObject.SetActive(true);
 		}
