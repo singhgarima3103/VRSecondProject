258063dd3eac32d28a972a8e8723b400b5d5e019
singhgarima3103 <singhgarima3103@gmail.com>
2016-02-23 12:01:43 -0500

    tried adding fighting between t-rex and triceratops

---
+++ Assets/Scripts/AC/TrexAC.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrexAC : MonoBehaviour
+{
+
+    public float timeBetweenAttacks = 1f;
+
+    Animator anim;
+    LayerMask dinoMask;
+    LayerMask playerMask;
+    public GameObject enemyBody;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        dinoMask = LayerMask.NameToLayer("Dinosaur");
+        playerMask = LayerMask.NameToLayer("Player");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == enemyBody)
+        {
+            anim.SetTrigger("Attack");
+            anim.SetBool("HaveEnemy", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("HaveEnemy", false);
+    }
+    void FixedUpdate()
+    {
+
+    }
+}
