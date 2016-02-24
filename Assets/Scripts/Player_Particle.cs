d6e9f5178620d00498884d2b2a28ac5971b99f2f
gargankur2008 <gargankur20011@gmail.com>
2016-02-24 04:51:49 -0500

    Teleport done and Particle try

---
+++ Assets/Scripts/Player_Particle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Particle : MonoBehaviour {
+
+	public GameObject PartSys;
+	float timer,timer1;
+	// Use this for initialization
+	void Start () {
+	}
+	void OnTriggerEnter( Collider collider)
+	{
+		if (collider.gameObject.tag == "Player") {
+			collider.isTrigger (true);
+		} else
+			collider.isTrigger (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (collider.isTrigger == "true" && timer >= 0) {
+			timer = 5f;
+			PartSys.gameObject.SetActive (true);
+		}
+	}
+}
